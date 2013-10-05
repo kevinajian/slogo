2cdaf65d606b3021198ebbf673d8a528384231ee
CarlosReyesStoneham <cer26@duke.edu>
2013-10-05 15:49:46 -0400

    finished variable class

--- src/commands/basic_syntax/Variable.java
+++ src/commands/basic_syntax/Variable.java
@@ -1,5 +1,24 @@
 package commands.basic_syntax;
 
-public class Variable {
+import model.Model;
+import parser.Constants;
 
+public class Variable {
+	String variableName = null;
+	double variableValue = Constants.VARIABLE_DEFAULT;
+	
+	public String getVariableName() {
+		return variableName;
+	}
+	public void setVariableName(String variableName) {
+		this.variableName = variableName;
+	}
+	
+	public double getVariableValue() {
+		return variableValue;
+	}
+	public void setVariableValue(double variableValue) {
+		this.variableValue = variableValue;
+	}	
+		
 }
