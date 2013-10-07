9628d6712f25a2f310bf705ec676e7e1e4a9094e
CarlosReyesStoneham <cer26@duke.edu>
2013-10-07 01:29:14 -0400

    new parser implemented

---
+++ src/commands/vcu/ControlStructure.java
@@ -0,0 +1,15 @@
+package commands.vcu;
+
+import commands.Command;
+
+import model.Model;
+
+public class ControlStructure extends Command {
+
+	@Override
+	public double evaluate(Model model) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
