e2f63d15360d5a8d02a9918c940fe71abd8cadb9
kjian279 <kjian279@gmail.com>
2013-10-11 08:46:38 -0400

    changes to controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,11 @@
 package controller;
 import view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import parser.Constants;
 import model.Model;
 import model.State;
 
@@ -28,6 +30,21 @@ public class Controller {
 		myModel=model;
 	}
 	
+	public void processInput(String string) {
+		try {
+			myModel.processString(string);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<Line> trail = getLines();
+		double[] turtlePosition = getTurtle();
+		for (Line line:trail) {
+			//view.drawLine
+		}
+		//view.drawTurtle
+	}
+	
 	/**
 	 * passes user input to model to process
 	 * @param input - String of user input 
@@ -35,8 +52,6 @@ public class Controller {
 	 */
 	public void processString(String string) throws Exception{
 		myModel.processString(string);
-		//myModel.getOutput();
-		//myView.setOutput();
 	}
 	
 	/**
@@ -48,12 +63,33 @@ public class Controller {
 	}
 	
 	/**
-	 * gets trail
+	 * gets turtle states
 	 * @return List of Lines that create trail
 	 */
 	public List<State> getStates() {
 		myModel.createStates();
 		return myModel.getStates();
+	}
+	
+	public List<Line> getLines() {
+		List<State> states = getStates();
+		List<Line> lines = new ArrayList<Line>();
+		for (int i=0; i<states.size()-1; i++){
+		
+			Line line = new Line();
+			line.setCoord1(states.get(i).getX(), states.get(i).getY()); 
+			line.setCoord2(states.get(i+1).getX(), states.get(i+1).getY());
+			lines.add(line);
+		}
+		return lines;
+	}
+	
+	public double[] getTurtle() {
+		if (myModel.getCurrentState().getTurtleVisible().equals(Constants.TURTLE_NOTSHOWING)) return null;
+		else {
+			double[] coordinates = {myModel.getX(), myModel.getY(), myModel.getOrientation()};
+			return coordinates;
+		}
 	}
 	
 	public Map getVariables() {
