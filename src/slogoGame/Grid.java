f674f4d308d78c240af081d7597e4a65812c1945
Vincent Wang <vw22@duke.edu>
2013-10-11 04:49:40 -0400

    Fixing grid and added line

--- src/slogoGame/Grid.java
+++ src/slogoGame/Grid.java
@@ -1,17 +1,30 @@
 package slogoGame;
 
+import java.util.ArrayList;
+
 import jgame.*;
 import jgame.platform.JGEngine;
 
 public class Grid extends JGObject{
 	private JGEngine myEngine;
+	ArrayList<Line> l;
 	public Grid(String name, int collisionid, JGEngine tgame) {
 		super(name, true, tgame.getWidth(), tgame.getHeight(), collisionid, null);
 		myEngine = tgame;
+		l = new ArrayList<Line>();
 	}
 	public void paint()
 	{
-		myEngine.setColor(JGColor.red);
-		myEngine.drawLine(0, myEngine.getHeight()/2, myEngine.getWidth(), myEngine.getHeight()/2, 5, JGColor.red);//radius of 5
+		int num = 10;
+		int xNum = myEngine.getWidth()/num;
+		int yNum = myEngine.getHeight()/num;
+		for (int i = 0; i <= num; i++) //horizontal lines
+		{
+			l.add(new Line("line", 10, myEngine, 0, yNum*i, myEngine.getWidth(), yNum*i));
+		}
+		for (int i = 0; i <= num; i++) //vertical lines
+		{
+			l.add(new Line("line", 10, myEngine, xNum*i, 0, xNum*i, myEngine.getHeight()));
+		}
 	}
 }
