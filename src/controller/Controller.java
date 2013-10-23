9b54a5ddf16610320f29baa2331e4f9cc3dc355e
kjian279 <kjian279@gmail.com>
2013-10-23 03:28:24 -0400

    added clearBoxes method

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -49,7 +49,7 @@ public class Controller {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		clearLinesAndTurtles();
+		clearLinesAndTurtlesAndBoxes();
 		for (Model m: myParser.getModels().values()) {
 			List<Line> trail = getLines(m);
 
@@ -93,9 +93,14 @@ public class Controller {
 		myView.drawLine(currentLine);
 	}
 
-	private void clearLinesAndTurtles() {
+	private void clearLinesAndTurtlesAndBoxes() {
 		clearLines();
 		clearTurtles();
+		clearBoxes();
+	}
+
+	private void clearBoxes() {
+		myView.clearBoxes();
 	}
 
 	/**
