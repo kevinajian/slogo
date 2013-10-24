c8d7700187cbab7b762bdf3c8a5a516df225ebd3
CarlosReyesStoneham <cer26@duke.edu>
2013-10-23 22:39:02 -0400

    exception on class doesnt exist imp

--- src/commands/math/MathOneInput.java
+++ src/commands/math/MathOneInput.java
@@ -1,9 +1,15 @@
 package commands.math;
 
 import model.MathModel;
+import model.Model;
 import commands.Command;
 import commands.CommandOneInput;
 
 public abstract class MathOneInput extends CommandOneInput {
-
+	
+	@Override
+	public double evaluate(Model model) {
+		return myNumInputs;
+		
+	}
 }
