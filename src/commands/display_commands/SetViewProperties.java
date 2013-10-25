d5c54c1a2f1eb3bac9252047f6a3b50c31b41e60
CarlosReyesStoneham <cer26@duke.edu>
2013-10-24 20:08:33 -0400

    minor commenting

--- src/commands/display_commands/SetViewProperties.java
+++ src/commands/display_commands/SetViewProperties.java
@@ -6,14 +6,25 @@ import java.util.ResourceBundle;
 import model.Model;
 import commands.CommandOneInput;
 
+/**
+ * Takes care of porting arrays generated
+ * from properties file to arraylists for more flexibility.
+ * @author carlosreyes
+ *
+ */
 public class SetViewProperties extends CommandOneInput {
 	protected ResourceBundle myResources;
 	protected static final String DEFAULT_RESOURCE_PACKAGE = "resources.";
 	
+	/**
+	 * Pulls resource into array, and ports this to arrayList
+	 * @param model
+	 * @return ArrayList
+	 */
 	protected ArrayList<String> handleSet(Model model) {
 		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "English");
-		String lumpedColors = myResources.getString("Colors");
-		String colors1[] = lumpedColors.split(" ");
+		String resourceArray = myResources.getString("Colors");
+		String colors1[] = resourceArray.split(" ");
 		ArrayList<String> colors = new ArrayList<String>();
 		for(int i = 0; i<colors1.length-1;i++){
 			colors.add(colors1[i]);
